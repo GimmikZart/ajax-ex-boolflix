aca30f1fc5b40718d5994f93f11354bb7abb1ca4
GianmarcoMontanari <giammont@live.it>
2020-04-21 15:40:20 +0200

    Milestone 1
    Alla pressione del tasto cerca, viene stampato su browser una lista di film con titolo, anno, lingua e titolo originale presi dal server di "the movie database" tramite API

--- main.js
+++ main.js
@@ -4,3 +4,41 @@
 // Titolo Originale
 // Lingua
 // Voto
+
+
+$(document).ready(function(){
+
+  $("#bottone").on("click",function() {
+    $("#lista-film").html("");
+    var filtroUtente = $("#filtro-utente").val();
+    console.log(filtroUtente);
+    $.ajax({
+      url: 'https://api.themoviedb.org/3/search/movie',
+      data: {
+          api_key: 'ae4c927624a6458bc74bdd1b924c1cf1',
+          language: 'it-IT',
+          query: filtroUtente,
+        },
+      type: "GET",
+      dataType: "json",
+      success: function (data,stato) {
+          var listaFilm = data.results;
+          for (i = 0 ; i < listaFilm.length ; i++ ){
+            var titolo = listaFilm[i].title;
+            var titoloOriginale = listaFilm[i].original_title;
+            var lingua = listaFilm[i].original_language;
+            var voto = listaFilm[i].vote_average;
+            var source = $("#script-lista-film").html();
+            var template = Handlebars.compile(source);
+            var context = { titolo: titolo, titoloOriginale: titoloOriginale, lingua:lingua, voto: voto };
+            var html = template(context);
+
+            $("#lista-film").append(html);
+          } // fine ciclo for
+        }, // fine attributo success
+      error: function (error) {
+          console.log("Error ${error}");
+      },// fine attributo error
+    });
+  });
+});
